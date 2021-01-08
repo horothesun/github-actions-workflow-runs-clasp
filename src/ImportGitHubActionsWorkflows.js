0e1c5b586b05f70f46aee93a4fc0e81d18eff3f2
horothesun <horothesun@gmail.com>
2021-01-08 22:41:11 +0000

    CI test fix

--- src/ImportGitHubActionsWorkflows.js
+++ src/ImportGitHubActionsWorkflows.js
@@ -4,40 +4,40 @@ function getConfig() {
   }
 }
 
-function IMPORTGITHUBACTIONSWORKFLOWS(owner, repo, perPage) {
-  try {
-    let url = workflowsUrl(owner, repo, perPage)
-    let workflows = fetchWorkflows(url)
-    return workflowsTable(workflows)
-  } catch(err) {
-    return `Error getting data: ${err}`
-  }
-}
+// function IMPORTGITHUBACTIONSWORKFLOWS(owner, repo, perPage) {
+//   try {
+//     let url = workflowsUrl(owner, repo, perPage)
+//     let workflows = fetchWorkflows(url)
+//     return workflowsTable(workflows)
+//   } catch(err) {
+//     return `Error getting data: ${err}`
+//   }
+// }
 
-function workflowsUrl(owner, repo, perPage) {
-  return `https://api.github.com/repos/${owner}/${repo}/actions/workflows?per_page=${perPage}`
-}
+// function workflowsUrl(owner, repo, perPage) {
+//   return `https://api.github.com/repos/${owner}/${repo}/actions/workflows?per_page=${perPage}`
+// }
 
-function fetchWorkflows(url) {
-  let headers = {
-    'Accept': 'application/vnd.github.v3+json',
-    'Authorization': `token ${getConfig().workflow_read_token}`
-  }
-  let response = UrlFetchApp.fetch(url, { headers: headers })
-  let content = response.getContentText()
-  return JSON.parse(content).workflows
-}
+// function fetchWorkflows(url) {
+//   let headers = {
+//     'Accept': 'application/vnd.github.v3+json',
+//     'Authorization': `token ${getConfig().workflow_read_token}`
+//   }
+//   let response = UrlFetchApp.fetch(url, { headers: headers })
+//   let content = response.getContentText()
+//   return JSON.parse(content).workflows
+// }
 
-function workflowsTable(workflows) {
-  var rows = [ ['id', 'name', 'path', 'state', 'created_at', 'updated_at'] ]
-  for (i = 0; i < workflows.length; i++) {
-    let workflow = workflows[i]
-    let createdAtDate = new Date(workflow.created_at)
-    let updatedAtDate = new Date(workflow.updated_at)
-    rows.push([workflow.id, workflow.name, workflow.path, workflow.state, createdAtDate, updatedAtDate])
-  }
-  return rows
-}
+// function workflowsTable(workflows) {
+//   var rows = [ ['id', 'name', 'path', 'state', 'created_at', 'updated_at'] ]
+//   for (i = 0; i < workflows.length; i++) {
+//     let workflow = workflows[i]
+//     let createdAtDate = new Date(workflow.created_at)
+//     let updatedAtDate = new Date(workflow.updated_at)
+//     rows.push([workflow.id, workflow.name, workflow.path, workflow.state, createdAtDate, updatedAtDate])
+//   }
+//   return rows
+// }
 
 // console.log(IMPORTGITHUBACTIONSWORKFLOWS('horothesun', 'python-sample', 100))
 
